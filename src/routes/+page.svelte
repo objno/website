d9b5e2f4089bd33c13003fc7bf38c2898845e4eb
Simen A. W. Olsen <so@bjerk.io>
2023-01-29 15:20:24 +0100

    improve typing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="content">
-	<strong>We change the world to a better place by gathering passionate people.</strong>
+	<strong>We make the world a better place by gathering passionate people.</strong>
 	<p>
 		We build fantastic companies, such as <a href="https://ayr.no">AYR</a>,
 		<a href="https://bjerk.io">Bjerk</a>, <a href="https://tabetalt.no">Tabetalt</a>,
